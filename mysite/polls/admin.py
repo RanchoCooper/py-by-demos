32dc0bca4c8cda11597cf2147b48d177340878f9
Rancho <ranchocooper@gmail.com>
2025-02-05 17:24:51 +0800

    feat: add django example

---
+++ mysite/polls/admin.py
@@ -0,0 +1,22 @@
+from django.contrib import admin
+from .models import Question, Choice
+
+# Register your models here.
+
+class ChoiceInline(admin.TabularInline):
+    model = Choice
+    extra = 3
+
+class QuestionAdmin(admin.ModelAdmin):
+    # fields = ["pub_date", "question_text"]
+    fieldsets = [
+        (None, {"fields": ["question_text"]}),
+        ("Date information", {"fields": ["pub_date"]}),
+    ]
+    inlines = [ChoiceInline]
+    list_display = ("question_text", "pub_date", "was_published_recently")
+    list_filter = ["pub_date"]
+    search_fields = ["question_text"]
+    list_per_page = 10
+
+admin.site.register(Question, QuestionAdmin)
